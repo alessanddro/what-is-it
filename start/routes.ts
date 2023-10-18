048c23b246b8fa95f87dc2181732325959a65c69
Alessandro <alessanddro.brito@gmail.com>
2023-10-18 19:06:48 -0300

    Create Auth Route and Controller

--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.group(() => {
+  Route.resource('auth', 'AuthController').apiOnly()
 })
 Route.group(() => {
-  Route.resource('places', 'PlaceController')
-  Route.resource('sponsors', 'SponsorsController')
-  Route.resource('comments', 'CommentsController')
-  Route.resource('users', 'UserController')
+  Route.resource('places', 'PlaceController').apiOnly()
+  Route.resource('sponsors', 'SponsorsController').apiOnly()
+  Route.resource('comments', 'CommentsController').apiOnly()
+  Route.resource('users', 'UserController').apiOnly()
 }).prefix('api/v1')
